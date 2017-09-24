936c0ca7cb6418b0ad2b975e4287b40759f13997
Hernan Dario Cano <dcanohdev@gmail.com>
2017-09-24 01:51:10 -0500

    Cleaning (lide.platform.init)

--- platform/init.lua
+++ platform/init.lua
@@ -1,12 +1,34 @@
 lide.platform = lide.platform
-lide.platform.__arch = os.getenv 'PROCESSOR_ARCHITECTURE' 
-					   or io.popen 'uname -m' : read '*a' : gsub ('x64_64', 'x64') : gsub ( 'i686', 'x86' )
+--lide.platform.__arch = os.getenv 'PROCESSOR_ARCHITECTURE' 
+--					   or io.popen 'uname -m' : read '*a' : gsub ('x64_64', 'x64') : gsub ( 'i686', 'x86' )
 
-function lide.platform.getArch ()
-	return lide.platform.__arch
+--- Get the name of the operating system.
+---		Returns one string: OS Name like "Linux"
+---
+--- string getOSVersion( nil )
+function lide.platform.getOSName( ... )
+	if (package.config:sub(1,1) == '\\') and os.getenv 'OS' == 'Windows_NT' then
+		return 'Windows';
+	elseif (package.config:sub(1,1) == '/') and io.popen 'uname -s':read '*l' == 'Linux' then
+		return 'Linux';
+	else
+		return 'Other';
+	end
 end
 
-lide.platform.getOS = lide.platform.getOSName
+-- Get architecture of current OS
+-- string 'x86', 'x64'
+function lide.platform.getArch ()
+	local _osname = lide.platform.getOSName():lower()
+	if (_osname == 'windows') then
+		return os.getenv 'PROCESSOR_ARCHITECTURE' 
+	elseif (_osname == 'linux') then
+		return io.popen 'uname -m' : read '*a' : gsub ('x86_64', 'x64') : gsub ( 'i686', 'x86' );
+	end
+end
+
+lide.platform.getOS     = lide.platform.getOSName
+lide.platform.getOSArch = lide.platform.getArch
 
 if lide.platform.getOS() == 'Windows' then
 	windows = true 
