114c47786e488b6ad1156c7efdac882727d8e986
Hernan Dario Cano <dcanohdev@gmail.com>
2017-10-02 02:03:44 -0500

    Added (lide.file)

---
+++ classes/file.lua
@@ -0,0 +1,65 @@
+-- /////////////////////////////////////////////////////////////////////////////
+-- // Name:        lide/classes/file.lua
+-- // Purpose:     File class
+-- // Author:      Hernan Dario Cano [dcanohdev@gmail.com]
+-- // Created:     07/08/2017
+-- // Copyright:   (c) 2014 - 2017 Hernan Dario Cano
+-- // License:     GNU GENERAL PUBLIC LICENSE
+-- /////////////////////////////////////////////////////////////////////////////
+--
+-- Class constructor:
+--
+--  object File:new ( string filePath )
+--
+--  	string filePath		The object name
+--
+--
+
+-- import local classes:
+local Object   = lide.classes.object
+
+-- import functions:
+local isString = lide.core.base.isstring
+
+-- define the class:
+local File = class 'File' : subclassof 'Object' : global(false)
+
+function File:File ( strFilePath )
+	isString(strFilePath);
+
+	self._fullPath = strFilePath
+end
+
+function File:getFolder ( ... )
+	local splitPath = { string.match(self._fullPath, "(.-)([^\\/]-%.?([^%.\\/]*))$") }
+	
+	return splitPath [1]:sub(1, #splitPath[1] -1)
+end
+
+function File:getFilename ( ... )
+	local splitPath = { string.match(self._fullPath, "(.-)([^\\/]-%.?([^%.\\/]*))$") }
+	
+	return splitPath [2]
+end
+
+function File:getExtension ()
+	local splitPath = { string.match(self._fullPath, "(.-)([^\\/]-%.?([^%.\\/]*))$") }
+
+	return splitPath[3]
+end
+
+function File:__tostring ( ... )
+	return tostring(self._fullPath)
+end
+
+function File:getFullPath ( ... )
+	return self._fullPath
+end
+
+setmetatable(lide.file, { 
+	__call = function ( self, ... )
+	   return File (...)
+	end
+});
+
+return File
