66e8c054731335a4b89110cacc182d35245d37f6
Hernan Dario Cano <dcanohdev@gmail.com>
2017-11-23 16:58:50 -0500

    Update framework initializers

--- init.lua
+++ init.lua
@@ -7,8 +7,6 @@
 -- // License:     MIT License/X11 license
 -- /////////////////////////////////////////////////////////////////////////////////////////////////
 --
--- GNU/Linux Lua version:   5.1.5
--- Windows x86 Lua version: 5.1.4
 
 lide = require 'lide.core.init'
 app  = lide.app
@@ -19,11 +17,6 @@ lide.file = lide.core.file
 ------------------------------------------
 lide.core.base.maxid = 1000
 ------------------------------------------
-
--- int wxMessageBox(const wxString& message, const wxString& caption = "Message", int style = wxOK | wxCENTRE, wxWindow *parent = NULL, int x = -1, int y = -1 ); 
-function lide.core.base.messagebox ( message, caption, style, pos_x, pos_y, parent)
-	return wx.wxMessageBox(message or "", caption or "Message", style or wx.wxOK + wx.wxCENTRE, parent or wx.NULL, pos_x or -1, pos_y or -1 )	
-end
 
 -- Import classes to the framework:
 lide.classes = require 'lide.classes.init'
