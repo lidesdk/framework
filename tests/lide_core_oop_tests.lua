25aaa1b9b86455f46366630d8383c528184a7587
Hernan Dario Cano <dcanohdev@gmail.com>
2017-11-23 16:48:27 -0500

    Update OOP tests, add more complex validators

--- tests/lide_core_oop_tests.lua
+++ tests/lide_core_oop_tests.lua
@@ -1,15 +1,81 @@
-#!/usr/bin/env lua5.1
+-- -x create class
+-- -x instance class
+-- -x getters
+-- -x setters
+-- -x access modifiers
+-- -x methods
+-- - heritage
 
--- load only core lua framework without classes
-local lide = require 'lide.core.init'
-
--- Testear si existe la palabra clave 'class'
 io.stdout:write 'classes tests: '
 
-assert(type(class) == 'function', 'No existe la palabra clave class')
--- Testear si se pueden crear clases
-assert(type(class 'NEWCLASS') == 'table', 'No se pueden crear clases')
--- Testear si se pueden instanciar clases
-assert(type(class 'NEWCLASS':new()) == 'table', 'No se pueden instanciar clases')
+
+	-- load only core lua framework:
+	class = require 'lide.core.init' .class
+	
+assert(type(class) == 'function', 'lide.core.oop: keyword class (or lide.class) was not loaded.')
+
+	-- create class
+	local newClass = class 'newClass'
+
+assert(tostring(newClass) == 'class newClass', 'lide.core.oop: error creating class')
+
+	newClass:enum {
+		CONST = 111,
+	}
+
+assert(newClass.CONST == 111, 'lide.core.oop: error creating class constant')
+
+	function newClass:newClass ( ... )
+		protected {
+			value = 0,
+		}
+		
+		private {
+			_value = 1,
+		}
+
+		public {
+			prop = 'type12'
+		}
+	end
+
+	function newClass:get_value( ... )
+		return self.value 
+	end
+	
+	function newClass:set_value( value )
+		self.value = value
+		return self.value == value
+	end
+	
+	function newClass:get_pvalue( ... )
+		return self._value 
+	end
+	
+	function newClass:set_pvalue( value )
+		self._value = value
+		return self._value == value
+	end
+
+	-- instance class
+	local newInstance = newClass:new()
+
+assert(newInstance, 'lide.core.oop: error instantiating class')
+
+-- getters:
+assert(newInstance:get_value(), 'lide.core.oop: error calling get method')
+	
+-- setters:
+assert(newInstance:set_value(3), 'lide.core.oop: error calling set method')
+assert(newInstance:get_value() == 3, 'lide.core.oop: error protected values cant be accesed?')
+assert(newInstance:set_value(6), 'lide.core.oop: error protected values cant be modified?')
+
+assert(newInstance:set_pvalue(9), 'lide.core.oop: error private values cant be modified?')
+assert(newInstance:get_pvalue() == 9, 'lide.core.oop: error private values cant be accesed?')
+
+assert(newInstance.prop == 'type12', 'lide.core.oop: error public values cant be accesed?')
+
+assert(newInstance.value == nil, 'lide.core.oop: error protected values are exposed!')
+assert(newInstance._value == nil, 'lide.core.oop: error private values are exposed!')
 
 io.stdout:write '[OK]'
